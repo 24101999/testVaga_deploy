5d9585caed312e434c3782a8198d56e1f76a2e18
Henrique <henriquedasilvacosta@live.com>
2023-05-18 09:14:57 -0300

    finalizado

--- back/admin/gerar.php
+++ back/admin/gerar.php
@@ -5,105 +5,329 @@ require_once '../db.php';
 $gerar = $conn->prepare("INSERT INTO mesa_1 (nome , hora , dia) values (:nome , :hora , :dia)");
 
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
+$gerar->bindValue(':dia', 'segunda');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_1 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_1 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_2 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'henrique');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_2 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_2 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_3 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_3 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_3 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_4 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_4 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_4 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_5 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_5 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_5 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_6 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_6 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_6 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_7 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_7 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_7 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_8 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_8 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_8 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_9 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_9 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_9 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_10 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_10 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_10 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_10 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_11 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_11 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_11 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_12 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_12 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_12 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_13 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_13 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_14 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_14 (nome , hora , dia) values (:nome , :hora , :dia)");
 
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_14 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
+
+$gerar->execute();
+
+$gerar = $conn->prepare("INSERT INTO mesa_15 (nome , hora , dia) values (:nome , :hora , :dia)");
+
 $gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
+$gerar->bindValue(':hora', '19:30:00');
 $gerar->bindValue(':dia', 'segunda');
 
 $gerar->execute();
 $gerar = $conn->prepare("INSERT INTO mesa_15 (nome , hora , dia) values (:nome , :hora , :dia)");
 
-$gerar->bindValue(':nome', 'henrique');
-$gerar->bindValue(':hora', '14:00:00');
-$gerar->bindValue(':dia', 'segunda');
+$gerar->bindValue(':nome', 'lucas');
+$gerar->bindValue(':hora', '12:00:00');
+$gerar->bindValue(':dia', 'domingo');
+
+$gerar->execute();
+$gerar = $conn->prepare("INSERT INTO mesa_15 (nome , hora , dia) values (:nome , :hora , :dia)");
+
+$gerar->bindValue(':nome', 'larissa');
+$gerar->bindValue(':hora', '20:00:00');
+$gerar->bindValue(':dia', 'terca');
 
 $gerar->execute();
